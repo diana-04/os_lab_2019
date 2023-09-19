d1f5fd52a164b9ec66bf81a4737b474b35ae235a
diana-04 <104983937+diana-04@users.noreply.github.com>
2023-09-19 08:59:20 +0000

    Дописан файл revert_string.c

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "revert_string.h"
 
 void RevertString(char *str)
 {
-	// your code here
+	char buf;
+	for(int i = 0; i < strlen(str) / 2; i++)
+	{
+		buf = str[i];
+		str[i] = str[strlen(str) - 1 - i];
+		str[strlen(str) - 1 - i] = buf;
+	}
 }
 
